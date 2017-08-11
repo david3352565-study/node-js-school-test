6fe04e9eeda5f3b3d86910997265a9318e88a93a
david3352565-study <david3352565@gmail.com>
2017-08-11 14:49:44 +0300

    full validating, and changed mask

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ function MyForm(id) {
 	this.button = this.form.querySelector('#submitButton');
 	this.button.onclick = this.submit.bind(this);
 	this.phone.onfocus = function() {
-		VMasker(this).maskPattern('+9(999)999-99-99');
-		this.value = VMasker.toPattern('7', { pattern: '+9(999)999-99-99' });
+		var phoneNumber = new PhoneMask(this, {
+			pattern: '+7(___)___-__-__',
+			patternChar: '_',
+			allowedRegExp: /^\d$/
+		});
 	};
 }
 
@@ -25,68 +28,55 @@ MyForm.prototype.setData = function(obj) {
 	this.phone.value = obj.phone;
 };
 
+// Функция валидации
+MyForm.prototype.validate = function() {
+	let data = this.getData(),
+		isValid = true,
+		errorFields = [];
+	if (!this.nameValidating(data.fio)) {
+		isValid = false;
+		errorFields.push('fio');
+	} else this.fio.classList.remove('error');
+	if (!this.emailValidating(data.email)) {
+		isValid = false;
+		errorFields.push('email');
+	} else this.email.classList.remove('error');
+	if (!this.phoneValidating(data.phone)) {
+		isValid = false;
+		errorFields.push('phone');
+	} else this.phone.classList.remove('error');
+	if (errorFields.length > 0) {
+		errorFields.forEach(name => this.error(this[name]));
+	}
+	return { isValid: isValid, errorFields: errorFields };
+};
+
 MyForm.prototype.error = function(elem) {
 	elem.className += ' error';
 };
 
+// Валидация имени
 MyForm.prototype.nameValidating = function(nameValue) {
 	let arrNames = nameValue.split(' ').filter(el => {
 		return el.replace(/[^A-Za-zА-Яа-яё]/gim, '');
 	});
-	console.log(arrNames);
 	return arrNames.length == 3;
 };
 
+// Валидация email
 MyForm.prototype.emailValidating = function(emailValue) {
 	const pattern = /[a-zA-Z0-9.+@]+@(ya\.ru|(yandex\.(ru|ua|by|kz|com)))/;
 	return emailValue.match(pattern);
 };
 
+// Валидация телефона
 MyForm.prototype.phoneValidating = function(phoneValue) {
-	//console.log('im validating your phone');
-	return true;
+	let numArray = phoneValue.replace(/[^0-9]/gim, '').split(''),
+		sumNum = 0;
+	if (numArray.length != 11) return false;
+	numArray.forEach(elem => (sumNum += +elem));
+	return sumNum <= 30 ? true : false;
 };
-
-MyForm.prototype.innerValidate = function(boolCheck, isVal, arr, name) {
-	if (!boolCheck) {
-		isVal = false;
-		arr.push(name);
-	}
-};
-
-MyForm.prototype.validate = function() {
-	let data = this.getData(),
-		isValid = true,
-		errorFields = [];
-	console.log(data);
-	this.innerValidate(
-		this.nameValidating(data.fio),
-		isValid,
-		errorFields,
-		'fio'
-	);
-	this.innerValidate(
-		this.emailValidating(data.email),
-		isValid,
-		errorFields,
-		'email'
-	);
-	this.innerValidate(
-		this.phoneValidating(data.email),
-		isValid,
-		errorFields,
-		'email'
-	);
-	if (errorFields.length > 0) {
-		errorFields.forEach(name => this.error(this[name]));
-	} else {
-		for (let y in data) {
-			this[y].classList.remove('error');
-		}
-	}
-};
-
-MyForm.prototype.validatePhone = () => {};
 
 MyForm.prototype.submit = function() {
 	console.log(this.validate());
